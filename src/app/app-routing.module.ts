497419006115e88f11af8f540cba2a6fba62e9b8
Sebastian <sortizs@eafit.edu.co>
2018-12-21 20:22:47 -0500

    Ajustes de colores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,14 @@ import { EducationComponent } from './components/education/education.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 
 const APP_ROUTES: Routes = [
-  { path: 'home', component: HomeComponent },
+  // { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'education', component: EducationComponent },
   { path: 'experience', component: ExperienceComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' },
-  { path: '', pathMatch: 'full', redirectTo: 'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'about' },
+  { path: '', pathMatch: 'full', redirectTo: 'about' }
 ];
 
 @NgModule({
